b9ddad7b1b1628e86835d93f22cb50723e8076e2
MarkySarky <markjsantuyo@gmail.com>
2022-09-02 00:00:16 -0700

    name change

--- script.js
+++ script.js
@@ -33,15 +33,15 @@ function randomInt(min, max) {
       }
 
      }
-   var userWantsLowerCase = window.confirm ("Would you like to include Lowercase in your password?")
-   var userWantsUppercase = window.confirm ("Would you like to include UpperCase in your password?")
-   var userWantsNumbers = window.confirm ("Would you like to include numbers in your password?")
-   var userWantsSymbols = window.confirm ("Would you like to include symbols in your password?")
+   var userWantsLC = window.confirm ("Would you like to include Lowercase in your password?")
+   var useruWantsUC = window.confirm ("Would you like to include UpperCase in your password?")
+   var userWantsNUM = window.confirm ("Would you like to include numbers in your password?")
+   var userWantsSYM = window.confirm ("Would you like to include symbols in your password?")
 
-   var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-   var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "o", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-   var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-   var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+   var lcList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+   var ucList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "o", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+   var numList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+   var symList = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "|", "{", "}", "+", "=", "-" ]
 
 
    var stack = []
@@ -51,23 +51,23 @@ function randomInt(min, max) {
    }
 
    if (userWantsSymbols === true) {
-    stack.push(symbolList)
+    stack.push(symList)
    }
 
    if (userWantsLowerCase === true) {
-    stack.push(lowercaseList)
+    stack.push(lcList)
    }
 
    if (userWantsUppercase === true) {
-    stack.push(uppercaseList)
+    stack.push(ucList)
    }
 
    if (userWantsNumbers === true){
-       stack.push(numberList)
+       stack.push(numList)
    }
 
    if (stack.length === 0) {
-    stack.push(lowercaseList)
+    stack.push(lcList)
    }
 
    var generatedPassword = ""
